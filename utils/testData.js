690b5b494dd6be2eeb57eb8990b9b7b603facbd5
periyasamymuthuqa-prog <periyasamymuthu.qa@gmail.com>
2026-07-17 16:56:54 +0530

    Create testData.js

---
+++ utils/testData.js
@@ -0,0 +1,16 @@
+const testData = {
+
+    validUser: {
+        username: "standard_user",
+        password: "secret_sauce"
+    },
+
+    checkoutUser: {
+        firstName: "Periyasamy",
+        lastName: "Muthu",
+        postalCode: "600001"
+    }
+
+};
+
+module.exports = testData;
